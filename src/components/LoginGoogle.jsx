04b234989fecdb80068d7459784befc86e2a9fc1
Ame-kv <amelyku0204@gmail.com>
2025-09-17 18:45:59 -0600

    ajustes

--- src/components/LoginGoogle.jsx
+++ src/components/LoginGoogle.jsx
@@ -9,7 +9,7 @@ const LoginGoogle = ({ onLoginSuccess }) => {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
 
-    
+      // user al back
       onLoginSuccess(user);
     } catch (error) {
       console.error("Error en login con Google:", error);
